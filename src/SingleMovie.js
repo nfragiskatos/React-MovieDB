34e92ceed03cca1a8e74a5d9192cc603082001f1
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-09 08:58:42 -0700

    Initial state and fetch for single movie
    - Grabbed ID from router params
    - Setup state for movie, isLoading, and error
    - Created async fetchMovie function to make a request to the API endpoint
    - Added useEffect to call fetchMovie with id state variable in the dependency list

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,9 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { API_ENDPOINT } from './context'
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { API_ENDPOINT } from './context';
 
 const SingleMovie = () => {
-  return <h2>single movie</h2>
-}
+	const { id } = useParams();
+	const [ movie, setMovie ] = useState({});
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [ error, setError ] = useState({ show: false, msg: '' });
 
-export default SingleMovie
+	const fetchMovie = async (url) => {
+		setIsLoading(true);
+		const response = await fetch(url);
+		const data = await response.json();
+
+		if (data.Response === 'True') {
+			setMovie(data);
+		} else {
+			setError({
+				show : true,
+				msg  : data.Error
+			});
+		}
+		setIsLoading(false);
+
+		console.log(data);
+	};
+
+	useEffect(
+		() => {
+			fetchMovie(`${API_ENDPOINT}&i=${id}`);
+		},
+		[ id ]
+	);
+	return <h2>single movie</h2>;
+};
+
+export default SingleMovie;
