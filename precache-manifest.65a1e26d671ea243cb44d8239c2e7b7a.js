e559d7ff6b8ad12bda56a29dc29a8ee272ecb416
root <root@NFrag-Home-Desktop.localdomain>
2021-05-15 09:02:46 -0700

    Updates

---
+++ precache-manifest.65a1e26d671ea243cb44d8239c2e7b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f364407b21a8376d1050376b477d2b72",
+    "url": "/React-MovieDB/index.html"
+  },
+  {
+    "revision": "cd40da1a6df5c2daca17",
+    "url": "/React-MovieDB/static/css/main.289c1fa7.chunk.css"
+  },
+  {
+    "revision": "9aff709ffce6dd2dd0a7",
+    "url": "/React-MovieDB/static/js/2.5b374e73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-MovieDB/static/js/2.5b374e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd40da1a6df5c2daca17",
+    "url": "/React-MovieDB/static/js/main.fcd69f12.chunk.js"
+  },
+  {
+    "revision": "01c2999282fdae9f9d76",
+    "url": "/React-MovieDB/static/js/runtime-main.93ca6d40.js"
+  }
+]);
